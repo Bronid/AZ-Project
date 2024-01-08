b6d4b3a6fb5ed0d652402c1c9f09895834172629
radusya <radulya777@icloud.com>
2024-01-08 19:11:34 +0100

    added activityManager class

---
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/Classes/ActivityManager.kt
@@ -0,0 +1,37 @@
+package com.google.codelabs.buildyourfirstmap.Classes
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+class ActivityManager {
+    private val scheduler = Executors.newSingleThreadScheduledExecutor()
+    private val interval = 5L
+
+    val activityList = listOf(
+        "Takes a stroll through the forest",
+        "Trips over a rock",
+        "Falls into a pit",
+        "Constructs a shelter from branches",
+        "Catches a fish in the river"
+    )
+
+    init {
+        leaveShelter()
+    }
+
+    fun leaveShelter() {
+        val activityTask = Runnable {
+            randomActivity()
+        }
+        scheduler.scheduleAtFixedRate(activityTask, 0, interval, TimeUnit.SECONDS)
+    }
+
+    fun joinShelter() {
+        scheduler.shutdown()
+    }
+
+    private fun randomActivity() {
+        val randomIndex = (0 until activityList.size).random()
+        val storyMessage = activityList[randomIndex]
+        println(storyMessage)
+    }
+}
